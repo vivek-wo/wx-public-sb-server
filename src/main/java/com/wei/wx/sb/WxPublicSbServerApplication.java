9549a4e3462234c52fa091e86c1cf573be15046e
wuwei1 <Ut123456>
2023-03-18 17:48:48 +0800

    Initial commit

---
+++ src/main/java/com/wei/wx/sb/WxPublicSbServerApplication.java
@@ -0,0 +1,13 @@
+package com.wei.wx.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxPublicSbServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WxPublicSbServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/wei/wx/sb/WxPublicSbServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wei.wx.sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxPublicSbServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
